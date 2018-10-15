564f06c66592d0b65c4212b8f48443072f41f795
William Dupuis <william.dupuis@tuxtelecom.net>
2018-10-15 16:46:51 +0200

    Typewriter text

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,22 @@
 "use strict"
 
-// Menu items
-$(document).ready( function (event) {
-    // console.log(event);
-    $('.nav-item').on("click", function () {
-        $('.nav-item').removeClass('active');
-        $(this).addClass('active');
-    });
+// console.log(event);
+$('.nav-item').on("click", function () {
+    $('.nav-item').removeClass('active');
+    $(this).addClass('active');
 });
+
+
+// Text writer
+var word  = $('#tap-text').text().trim();
+
+// Vider la div
+$('#tap-text').text('');
+
+// Parcourir les lettres du mot
+for (let i=0 ; i<word.length ; i++) {
+    console.log(word[i]);
+    setTimeout(function () {
+        $("#tap-text").append(word[i]);
+    }, 300 * i )
+}
